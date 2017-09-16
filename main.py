c97be44b4b5ee3411ec1d26bf5e99c64f1fc97b9
Cherie <c.k.fishlock@gmail.com>
2017-09-16 17:03:27 +1000

    Add metadata stripping

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
-"""
-Strips metadata from images.
-"""
-# import PIL
 from PIL import Image
 import piexif
 import argparse
+import glob
+import os
+import sys
 
-__author__ = "Cherie"
 
-
-def rotate_02(filename):
+def strip_metadata(filename):
     img1 = Image.open(filename)
+
     if "exif" in img1.info:
         exif_dict = piexif.load(img1.info["exif"])
+
     if piexif.ImageIFD.Orientation in exif_dict["0th"]:
         orientation = exif_dict["0th"].pop(piexif.ImageIFD.Orientation)
         exif_bytes = piexif.dump(exif_dict)
 
-    #img = piexif.load(filename)
-
-    #print(piexif.VERSION)
     piexif.remove(filename)
 
     img = Image.open(filename)
-    print(img.info)
 
     if orientation == 2:
         img = img.transpose(Image.FLIP_LEFT_RIGHT)
@@ -40,17 +35,42 @@ def rotate_02(filename):
     elif orientation == 8:
         img = img.rotate(90)
 
-
-    img.save('out03.jpg') #, exif=exif_bytes)
-
+    return img
 
 
 if __name__ == "__main__":
-    # print(PIL.PILLOW_VERSION)
-    # print(piexif.VERSION)
-    # print(argparse.__version__)
 
-    rotate_02('test03.jpg')
+    if len(sys.argv) == 2:
+        path = str(sys.argv[1])
+    else:
+        path = ""
+
+    # Get path info
+    f = open(os.path.join(path,'image_path'),'r')
+    lines = f.readlines()
+    image_path = lines[0].replace('\n','')
+    stripped_path = lines[1].replace('\n','')
+    f.close()
+
+    # Get list of images
+    img_list = glob.glob(os.path.join(image_path,'*'))
+
+    for img_name in img_list:
+
+        img_name = img_name.split('/')[-1]
+        
+        # Move image before stripping metadata
+        os.system('mv ' + os.path.join(image_path,img_name) + ' ' + os.path.join(stripped_path,img_name))
+
+        # Strip metadata and rotate image
+        img = strip_metadata(os.path.join(stripped_path,img_name))
+
+        # Save image
+        img.save(os.path.join(stripped_path,img_name),quality=95)
+
+
+
+
 
 
 
